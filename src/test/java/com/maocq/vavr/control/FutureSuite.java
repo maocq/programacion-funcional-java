cca340472300dd1b3d9625c1b1d97a28623701d9
maocq <mauriciocarmona@seven4n.com>
2019-02-19 09:32:39 -0500

    Future

---
+++ src/test/java/com/maocq/vavr/control/FutureSuite.java
@@ -0,0 +1,91 @@
+package com.maocq.vavr.control;
+
+import io.vavr.concurrent.Future;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FutureSuite {
+
+    @Test
+    public void testFuture() {
+        Future<String> future = Future.of(() -> {
+            Thread.sleep(500);
+            return "Ok";
+        });
+
+        assertEquals("", "Ok", future.get());
+    }
+
+    @Test(expected = Error.class)
+    public void testFutureWithError() {
+        Future<String> future = Future.of(() -> {
+            throw new Error("Failure");
+        });
+        future.get();
+    }
+
+    @Test
+    public void testFutureMap() {
+        Future<String> future = Future.of(() -> "Ok");
+        Future<Integer> futureMap = future.map(String::length);
+        Integer numero = 2;
+
+        assertEquals("", numero, futureMap.get());
+    }
+
+    @Test
+    public void testFutureFlatMap() {
+        Future<String> future = Future.of(() -> "Ok");
+        Future<Integer> futureFlatMap = future.flatMap(it -> Future.of(() -> it.length()));
+        Integer numero = 2;
+
+        assertEquals("", numero, futureFlatMap.get());
+    }
+
+    @Test
+    public void testFutureRecover() {
+        Future<String> future = Future.of(() -> {
+            throw new Error("Failure =(");
+        });
+        Future<String> futureRecover = future.recover(error -> error.getMessage());
+
+        assertEquals("", "Failure =(", futureRecover.get());
+    }
+
+    private Future<Integer> futures(String mensaje) {
+        return Future.of(() -> {
+            System.out.println("Inicio " + mensaje);
+            sleep(5000);
+            System.out.println("Fin " + mensaje);
+            return 1;
+        });
+    }
+
+    @Test
+    public void testFutures() {
+        Future<Integer> future1 = futures("Futuro 1");
+        Future<Integer> future2 = futures("Futuro 2");
+        Future<Integer> future3 = futures("Futuro 3");
+
+        Future<Integer> resultado = future1
+          .flatMap(uno -> future2
+          .flatMap(dos -> future3
+          .map(tres -> uno + dos + tres)));
+
+        Integer numero = 3;
+
+        assertEquals("", numero, resultado.get());
+    }
+
+
+
+    private void sleep(Integer tiempo) {
+        try {
+            Thread.sleep(tiempo);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
